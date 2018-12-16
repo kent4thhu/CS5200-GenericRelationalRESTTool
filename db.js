1e5658197304d70200b70f4d0d705ac2b0fc89c8
khu <sjw441500@gmail.com>
2018-12-16 17:36:15 -0500

    resolve one minor bug in db.js

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ module.exports = function () {
     const mongoose = require('mongoose');
     var connectionString='mongodb://localhost/white-board';//forlocal
     if (process.env.MONG_WEB_USERNAME) {
-        var connectionString =
+        connectionString =
             '@ds135704.mlab.com:35704/heroku_g4s147t1';
         const username = process.env.MONG_WEB_USERNAME;
         const password = process.env.MONG_WEB_PASSWORD;
