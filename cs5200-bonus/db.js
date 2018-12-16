c34112f8994d95d893b1795bb412cef2853ede66
khu <sjw441500@gmail.com>
2018-12-16 10:46:10 -0500

    config to enable run on heroku

--- cs5200-bonus/db.js
+++ cs5200-bonus/db.js
@@ -6,7 +6,9 @@ module.exports = function () {
     const mongoose = require('mongoose');
     const databaseName = 'white-board';
     var   connectionString =
-        'mongodb://localhost/';
-    connectionString += databaseName;
+        '@ds135704.mlab.com:35704/heroku_g4s147t1';
+    const username = process.env.MONG_WEB_USERNAME;
+    const password = process.env.MONG_WEB_PASSWORD;
+    connectionString = 'mongodb://' + username + ":" + password + connectionString;
     mongoose.connect(connectionString);
 };
