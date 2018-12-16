d29309fbc2bed0515e4931d819293f0464b18a63
khu <sjw441500@gmail.com>
2018-12-16 17:42:29 -0500

    update config;

--- db.js
+++ db.js
@@ -5,11 +5,11 @@
 module.exports = function () {
     const mongoose = require('mongoose');
     var connectionString='mongodb://localhost/white-board';//forlocal
-    if (process.env.MONG_WEB_USERNAME) {
+    if (process.env.MONGO_WEB_USERNAME) {
         connectionString =
             '@ds135704.mlab.com:35704/heroku_g4s147t1';
-        const username = process.env.MONG_WEB_USERNAME;
-        const password = process.env.MONG_WEB_PASSWORD;
+        const username = process.env.MONGO_WEB_USERNAME;
+        const password = process.env.MONGO_WEB_PASSWORD;
         connectionString = 'mongodb://' + username + ":" + password + connectionString;
     }
     else{
